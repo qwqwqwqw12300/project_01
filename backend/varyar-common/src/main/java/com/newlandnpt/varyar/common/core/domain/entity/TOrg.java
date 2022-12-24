d251c2d87140d005909724756d482394fb27bd93
linj <1017541912@qq.com>
2022-12-24 16:26:39 +0800

    1、部门相关替换为机构

--- backend/varyar-common/src/main/java/com/newlandnpt/varyar/common/core/domain/entity/TOrg.java
+++ backend/varyar-common/src/main/java/com/newlandnpt/varyar/common/core/domain/entity/TOrg.java
@@ -144,8 +144,6 @@ public class TOrg extends BaseEntity
         this.leaderPhone = leaderPhone;
     }
 
-    @Email(message = "邮箱格式不正确")
-    @Size(min = 0, max = 50, message = "邮箱长度不能超过50个字符")
     public String getType()
     {
         return type;
