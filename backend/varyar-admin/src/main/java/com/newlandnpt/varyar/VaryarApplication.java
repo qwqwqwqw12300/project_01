b004a3328ff8c102c246aa5daf9f0348062b4554
wangwa <65490758@qq.com>
2022-12-24 13:49:26 +0800

    init

---
+++ backend/varyar-admin/src/main/java/com/newlandnpt/varyar/VaryarApplication.java
@@ -0,0 +1,21 @@
+package com.newlandnpt.varyar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * 启动程序
+ * 
+ * @author newlandnpt
+ */
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+public class VaryarApplication
+{
+    public static void main(String[] args)
+    {
+        // System.setProperty("spring.devtools.restart.enabled", "false");
+        SpringApplication.run(VaryarApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  varyar启动成功   ლ(´ڡ`ლ)ﾞ  \n");
+    }
+}
