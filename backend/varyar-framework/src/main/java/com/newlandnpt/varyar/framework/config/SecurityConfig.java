0a293832021a3c9ba899f69860d71ae5a065d0f3
chenxw1 <782631635@qq.com>
2023-01-03 17:25:21 +0800

    [提交人：chenxw] 功能：重置更新密码功能

--- backend/varyar-framework/src/main/java/com/newlandnpt/varyar/framework/config/SecurityConfig.java
+++ backend/varyar-framework/src/main/java/com/newlandnpt/varyar/framework/config/SecurityConfig.java
@@ -113,7 +113,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
                 // 对于登录login 注册register 验证码captchaImage 允许匿名访问
                 .antMatchers("/login", "/register", "/captchaImage").permitAll()
                 // api端允许匿名访问的接口
-                .antMatchers("/api/captchaImage","/api/sendSms","/api/regMember","/api/loginBySms","/api/loginByPwd").permitAll()
+                .antMatchers("/api/captchaImage","/api/sendSms","/api/regMember","/api/loginBySms","/api/loginByPwd","/api/resetMemberPwd").permitAll()
                 // 静态资源，可匿名访问
                 .antMatchers(HttpMethod.GET, "/", "/*.html", "/**/*.html", "/**/*.css", "/**/*.js", "/profile/**").permitAll()
                 .antMatchers("/swagger-ui.html", "/swagger-resources/**", "/webjars/**", "/*/api-docs", "/druid/**").permitAll()
