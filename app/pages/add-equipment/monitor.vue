c7f60b2c03204445e7f6ad7bab412cfa5e4b734c
9546332 <qiukyh@user.noreply.gitee.com>
2023-01-03 14:02:19 +0800

    feat: 添加雷达波详情页

--- app/pages/add-equipment/monitor.vue
+++ app/pages/add-equipment/monitor.vue
@@ -1,10 +1,12 @@
 <!-- 增加监护设备 -->
 <template>
 	<app-body>
-		<view class="ui-navbar" @tap="handleBack">
+		<!-- <view class="ui-navbar" @tap="handleBack">
 			<u-icon name="arrow-left" color="#fff" size="23"></u-icon>
 			<text class="ui-navbar-text">首页</text>
-		</view>
+		</view> -->
+		<u-navbar leftText="首页" @rightClick="handleBack" :autoBack="true" bgColor="transparent" leftIconColor="#fff"
+			:fixed="false" />
 		<view class="ui-title">
 			<text>增加监护设备</text>
 		</view>
