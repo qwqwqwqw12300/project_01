75c85a0982df1aceeb20d4001260c2502b1b24b9
linj <1017541912@qq.com>
2023-01-10 10:05:17 +0800

    1、后台管理界面若依相关文字描述修改为varyar 2、管理系统logo替换 3、用户个人信息界面调整部门改为机构，去除邮箱

--- frontend/src/views/system/user/profile/userInfo.vue
+++ frontend/src/views/system/user/profile/userInfo.vue
@@ -2,13 +2,13 @@
   <el-form ref="form" :model="user" :rules="rules" label-width="80px">
     <el-form-item label="用户昵称" prop="nickName">
       <el-input v-model="user.nickName" maxlength="30" />
-    </el-form-item> 
+    </el-form-item>
     <el-form-item label="手机号码" prop="phonenumber">
       <el-input v-model="user.phonenumber" maxlength="11" />
     </el-form-item>
-    <el-form-item label="邮箱" prop="email">
-      <el-input v-model="user.email" maxlength="50" />
-    </el-form-item>
+<!--    <el-form-item label="邮箱" prop="email">-->
+<!--      <el-input v-model="user.email" maxlength="50" />-->
+<!--    </el-form-item>-->
     <el-form-item label="性别">
       <el-radio-group v-model="user.sex">
         <el-radio label="0">男</el-radio>
