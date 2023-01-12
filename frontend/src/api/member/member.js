c2aeb37a94a37157bad15c61e8cf9ace42c31d53
chenxw1 <782631635@qq.com>
2023-01-12 17:37:50 +0800

    [提交人：chenxw] 功能：1.新增会员管理页面 2.会员名下所有设备总览(家庭组信息未实现)3新增字典sql

--- frontend/src/api/member/member.js
+++ frontend/src/api/member/member.js
@@ -1,6 +1,15 @@
 import request from '@/utils/request'
 
-// 查询部门详细
+// 查询会员列表
+export function listMember(query) {
+  return request({
+    url: '/member/page',
+    method: 'get',
+    params: query
+  })
+}
+
+// 查询会员详细
 export function getMember(memberId) {
   return request({
     url: '/member/' + memberId,
