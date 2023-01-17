e87e9c7213f7e746ce0f2b79e7134b509540b40a
zhanch <851995277@qq.com>
2023-01-17 17:31:40 +0800

    （*）接口联调

--- app/pages/equipment/watch-detail.vue
+++ app/pages/equipment/watch-detail.vue
@@ -8,13 +8,19 @@
 				<view class="info">
 					<text class="position">{{ deviceInfo.location || '--' }}</text>
 					<view class="status">
-						<u-icon :name="deviceInfo.onlineFlag === '1' ? 'wifi' : 'wifi-off'" :color="deviceInfo.onlineFlag === '1' ? '#0dab1c' : '#ff4800'" size="48rpx"></u-icon>
+						<u-icon :name="deviceInfo.onlineFlag === '1' ? 'wifi' : 'wifi-off'"
+							:color="deviceInfo.onlineFlag === '1' ? '#0dab1c' : '#ff4800'" size="48rpx"></u-icon>
 						<!-- <image src="../../static/images/online.png"></image> -->
-						<text :style="{ color: deviceInfo.onlineFlag === '1' ? '#0dab1c' : '#ff4800' }">{{ deviceInfo.onlineFlag === '1' ? '在线' : '离线' }}</text>
+						<text
+							:style="{ color: deviceInfo.onlineFlag === '1' ? '#0dab1c' : '#ff4800' }">{{ deviceInfo.onlineFlag === '1' ? '在线' : '离线' }}</text>
 					</view>
 				</view>
 			</view>
-			<u-icon name="map-fill" @click="goMap" size="30" color="#FF7E00" class="ui-detail-map"></u-icon>
+			<view class="ui-icon">
+				<u-icon name="map-fill" @click="goMap" size="30" color="#FF7E00" class="ui-detail-map"></u-icon>
+				<u-icon name="map-fill" @click="goMap" size="30" color="#FF7E00" class="ui-detail-map"></u-icon>
+			</view>
+
 		</view>
 		<view class="ui-msg-list">
 			<msg-list @navClick="onNavClick" @onRefresh="onRefresh" :list="messageList"
@@ -24,158 +30,172 @@
 </template>
 
 <script>
-import { mapState } from 'vuex';
-import { getMessage, PostMessageDeatil } from '../../common/http/api';
-export default {
-	data() {
-		return {
-			list1: [
-				{
-					name: '未读'
-				},
-				{
-					name: '已读'
-				}
-			],
-			/**消息列表**/
-			messageList: [],
-			navActive: 0
-		};
-	},
-	computed: {
-		...mapState({
-			deviceInfo: state => state.deviceInfo
-		})
-	},
-	mounted() {
-		this.getMessage();
-	},
-	methods: {
-		getMessage() {
-			return new Promise(resolve => {
-				const { type, familyId, deviceId } = this.deviceInfo;
-				getMessage({
-					deviceType: type,
-					familyId,
-					deviceId,
-					readFlag: this.navActive,
-					eventlevel: ''
-				}).then(res => {
-					this.messageList = res.rows || [];
-					resolve(this.messageList);
-				});
-			});
+	import {
+		mapState
+	} from 'vuex';
+	import {
+		getMessage,
+		PostMessageDeatil
+	} from '../../common/http/api';
+	export default {
+		data() {
+			return {
+				list1: [{
+						name: '未读'
+					},
+					{
+						name: '已读'
+					}
+				],
+				/**消息列表**/
+				messageList: [],
+				navActive: 0
+			};
 		},
-
-		/**
-		 * 上拉刷新
-		 * @param {Object} $e
-		 */
-		onRefresh(cb) {
-			this.getMessage().then(res => cb());
+		computed: {
+			...mapState({
+				deviceInfo: state => state.deviceInfo
+			})
 		},
-
-		/**
-		 * tab切换
-		 */
-		onNavClick(index) {
-			this.navActive = index;
+		mounted() {
 			this.getMessage();
 		},
-		
-		/**
-		 * 跳转地图
-		 */
-		goMap() {
-			uni.navigateTo({
-				url: '/pages/equipment/map'
-			})
+		methods: {
+			getMessage() {
+				return new Promise(resolve => {
+					const {
+						type,
+						familyId,
+						deviceId
+					} = this.deviceInfo;
+					getMessage({
+						deviceType: type,
+						familyId,
+						deviceId,
+						readFlag: this.navActive,
+						eventlevel: ''
+					}).then(res => {
+						this.messageList = res.rows || [];
+						resolve(this.messageList);
+					});
+				});
+			},
+
+			/**
+			 * 上拉刷新
+			 * @param {Object} $e
+			 */
+			onRefresh(cb) {
+				this.getMessage().then(res => cb());
+			},
+
+			/**
+			 * tab切换
+			 */
+			onNavClick(index) {
+				this.navActive = index;
+				this.getMessage();
+			},
+
+			/**
+			 * 跳转地图
+			 */
+			goMap() {
+				uni.navigateTo({
+					url: '/pages/equipment/map'
+				})
+			}
 		}
-	}
-};
+	};
 </script>
 
 <style lang="scss" scoped>
-.wd-box {
-	border-radius: 10rpx;
-	margin: 0 46rpx;
-	// margin-top: 20rpx;
-	background-color: #ebebeb;
-}
-
-.ui-msg-list {
-	padding: 0 46rpx;
-}
-
-.ui-navbar {
-	padding-top: calc(var(--status-bar-height) + 26rpx);
-	padding-left: 20rpx;
-	display: flex;
-	align-items: center;
-
-	.ui-navbar-text {
-		font-size: 36rpx;
-		color: #fff;
-	}
-}
-
-.ui-detail {
-	@extend .wd-box;
-	margin-top: 40rpx;
-	padding: 40rpx;
-	position: relative;
-	display: flex;
-
-	// justify-content: space-between;
-	.ui-detail-icon {
-		width: 180rpx;
-		height: 180rpx;
+	.wd-box {
+		border-radius: 10rpx;
+		margin: 0 46rpx;
+		// margin-top: 20rpx;
+		background-color: #ebebeb;
 	}
 
-	.ui-detail-device {
-		margin-left: 90rpx;
+	.ui-msg-list {
+		padding: 0 46rpx;
+	}
 
-		.title {
-			line-height: 44rpx;
+	.ui-navbar {
+		padding-top: calc(var(--status-bar-height) + 26rpx);
+		padding-left: 20rpx;
+		display: flex;
+		align-items: center;
+
+		.ui-navbar-text {
 			font-size: 36rpx;
-			font-weight: 550;
+			color: #fff;
+		}
+	}
+
+	.ui-detail {
+		@extend .wd-box;
+		margin-top: 40rpx;
+		padding: 40rpx;
+		position: relative;
+		display: flex;
+
+		// justify-content: space-between;
+		.ui-detail-icon {
+			width: 180rpx;
+			height: 180rpx;
 		}
 
-		.info {
-			margin-top: 16rpx;
-			width: 220rpx;
+		.ui-icon {
+			width: 200rpx;
 			display: flex;
-			justify-content: space-between;
+			flex-direction: row;
+		}
 
-			.position {
-				font-size: 32rpx;
+		.ui-detail-device {
+			margin-left: 90rpx;
+
+			.title {
+				line-height: 44rpx;
+				font-size: 36rpx;
 				font-weight: 550;
 			}
 
-			.status {
+			.info {
+				margin-top: 16rpx;
+				width: 220rpx;
 				display: flex;
+				justify-content: space-between;
 
-				image {
-					width: 40rpx;
-					height: 40rpx;
-				}
-
-				text {
+				.position {
 					font-size: 32rpx;
 					font-weight: 550;
-					color: #1d8d28;
-					margin-left: 6rpx;
+				}
+
+				.status {
+					display: flex;
+
+					image {
+						width: 40rpx;
+						height: 40rpx;
+					}
+
+					text {
+						font-size: 32rpx;
+						font-weight: 550;
+						color: #1d8d28;
+						margin-left: 6rpx;
+					}
 				}
 			}
 		}
-	}
 
-	.ui-detail-map {
-		position: absolute;
-		// width: 50rpx;
-		// height: 60rpx;
-		right: 20rpx;
-		bottom: 20rpx;
+		.ui-detail-map {
+			position: absolute;
+			// width: 50rpx;
+			// height: 60rpx;
+			right: 20rpx;
+			bottom: 20rpx;
+		}
 	}
-}
 </style>
