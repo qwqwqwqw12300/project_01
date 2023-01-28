2ee6cb00ab1b40d0594e7fd94a3774cf5c3f477d
linj <1017541912@qq.com>
2023-01-28 09:46:21 +0800

    1、添加事件等级字典表

--- doc/sql/03-varyar_dict_extra.sql
+++ doc/sql/03-varyar_dict_extra.sql
@@ -89,3 +89,10 @@ insert into sys_dict_data values(null, 1,  '电话通知',     '0',      'sys_se
 insert into sys_dict_type values(null, '设备在线状态', 'sys_online_flag',   '0', 'admin', sysdate(), '', null, '设备在线状态（0离线 1在线 ）');
 insert into sys_dict_data values(null, 1,  '离线',     '0',      'sys_online_flag',   '',   'info',  'N', '0', 'admin', sysdate(), '', null, '0:离线');
 insert into sys_dict_data values(null, 2,  '在线',     '1',      'sys_online_flag',   '',   'info',  'N', '0', 'admin', sysdate(), '', null, '1:在线');
+
+-- ----------------------------
+-- 事件等级（normal普通 urgent紧急 ）
+-- ----------------------------
+insert into sys_dict_type values(null, '事件等级', 'event_level',   '0', 'admin', sysdate(), '', null, '事件等级（normal普通 urgent紧急 ）');
+insert into sys_dict_data values(null, 1,  '普通',     'normal',      'event_level',   '',   'info',  'N', '0', 'admin', sysdate(), '', null, 'normal:普通');
+insert into sys_dict_data values(null, 2,  '紧急',     'urgent',      'event_level',   '',   'info',  'N', '0', 'admin', sysdate(), '', null, 'urgent:紧急');
