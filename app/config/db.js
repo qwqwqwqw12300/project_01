83f1e14e0e4dc2605c4d24c638e250b9ca4fa619
zhanch <851995277@qq.com>
2023-02-08 17:31:31 +0800

    （*）功能优化

--- app/config/db.js
+++ app/config/db.js
@@ -6,9 +6,9 @@ export const INIT_DEIVCE_SET = {
 	deviceNo: '',
 	familyId: '',
 	roomId: '',
-	roomLeft: 6,
+	roomLeft: 3,
 	roomHeight: 6,
-	roomRight: 6,
+	roomRight: 3,
 	roomLength: 6,
 	existFlag: 0,
 	fallFlag: 0,
@@ -18,4 +18,47 @@ export const INIT_DEIVCE_SET = {
 	endTime: 0,
 	inMonitorFlag: 0,
 	outMonitorFlag: 0,
+};
+
+/**监控子区域**/
+export const ZONE = {
+	zoneType: '0',
+	/**区域id**/
+	roomZoneId: '',
+	/**设备id**/
+	deviceId: '',
+	/**房间id**/
+	roomId: '',
+	/**区域名称**/
+	name: '',
+	/*监控类型 0-监控区域  1-私人区域**/
+	existFlag: 0,
+	/**跌倒监控**/
+	fallFlag: 0,
+	x1: 0,
+	x2: 0,
+	y1: 0,
+	y2: 0,
+	z1: 100,
+	z2: 100,
+	old: {
+		x: 0,
+		y: 0
+	},
+	x: 0,
+	y: 0,
+	width: '',
+	height: '',
+	/**进入报警区域时间**/
+	entryTime: null,
+	/**离开报警区域时间**/
+	departureTime: null,
+	/**开始监控时间**/
+	startTime: null,
+	/**结束监控时间**/
+	endTime: null,
+	/**进入监控区域报警**/
+	inMonitorFlag: 0,
+	/**离开监控区域报警**/
+	outMonitorFlag: 0
 };
