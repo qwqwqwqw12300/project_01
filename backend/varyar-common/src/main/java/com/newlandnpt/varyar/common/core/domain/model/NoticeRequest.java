cb66f859255dddc28b5894e2af1a0525dce700a1
chenxw1 <782631635@qq.com>
2023-02-17 16:33:02 +0800

    [提交人：chenxw] api swagger文档接口注释提交

--- backend/varyar-common/src/main/java/com/newlandnpt/varyar/common/core/domain/model/NoticeRequest.java
+++ backend/varyar-common/src/main/java/com/newlandnpt/varyar/common/core/domain/model/NoticeRequest.java
@@ -1,18 +1,23 @@
 package com.newlandnpt.varyar.common.core.domain.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
-
+@ApiModel(description = "获取公告请求")
 public class NoticeRequest {
 
     /**
      *  未读标识 0:未读 1:已读
      * */
+    @ApiModelProperty("未读标识 0:未读 1:已读")
     @Size(min = 0, max = 1, message = "未读标识不能超过1个字符")
     private String readFlag;
     /**
      *  公告id
      * */
+    @ApiModelProperty("公告id")
     @NotBlank(message = "公告id不能为空")
     @Size(min = 0, max = 20, message = "公告id不能超过20个字符")
     private String noticeId;
