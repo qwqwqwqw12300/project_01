cc9adc8c771f425ecf76804756f1eef026b4e703
linj <1017541912@qq.com>
2023-02-22 14:44:21 +0800

    1、vayyar设备参数下发 mq增加tag为vayyar,增加key为设备id 2、cloud 接受设备参数下发mq 增加tag为vayyar

--- backend/varyar-cloud/src/main/java/com/newlandnpt/varyar/cloud/controller/rocketmq/DeviceConfigListener.java
+++ backend/varyar-cloud/src/main/java/com/newlandnpt/varyar/cloud/controller/rocketmq/DeviceConfigListener.java
@@ -6,6 +6,7 @@ import java.util.concurrent.TimeUnit;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.client.producer.SendStatus;
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
+import org.apache.rocketmq.spring.annotation.SelectorType;
 import org.apache.rocketmq.spring.core.RocketMQListener;
 import org.apache.rocketmq.spring.core.RocketMQTemplate;
 import org.slf4j.Logger;
@@ -32,7 +33,8 @@ import com.newlandnpt.varyar.common.utils.spring.SpringUtils;
  * @date 2023-01-11
  */
 @Component
-@RocketMQMessageListener(topic = "${rocketmq.topic.deviceConfig}", consumerGroup = "${rocketmq.group.deviceConfig}")
+@RocketMQMessageListener(topic = "${rocketmq.topic.deviceConfig}", consumerGroup = "${rocketmq.group.deviceConfig}",
+		selectorType= SelectorType.TAG,selectorExpression="vayyar")
 public class DeviceConfigListener implements RocketMQListener<DeviceConfig> {
 
 	private static final Logger log = LoggerFactory.getLogger(DeviceConfigListener.class);
