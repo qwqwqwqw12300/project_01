23fb9fce35d8bbbb8be9992f1452f161da12fd5b
youh <youh123!>
2023-02-23 15:53:33 +0800

    chore: 房间管理样式调整

--- app/pages/myself/room-manage.vue
+++ app/pages/myself/room-manage.vue
@@ -7,36 +7,45 @@
 
 <template>
 	<view id="roomMagege">
-		<app-body>
-			<app-logo :text="familyInfo.name">
-				<u-icon class="ui-logo-icon" name="edit-pen" size="60rpx" color="#fff" @click="openFamilyEdit"></u-icon>
-			</app-logo>
-			<view class="ui-menu">
-				<u-grid col="2">
-					<u-grid-item v-for="(item, index) in list" :key="index">
-						<view class="ui-menu-item active" :border="false" @click="openRoomEdit(item)">
-							<u-icon :customStyle="{ paddingTop: 20 + 'rpx' }" name="/static/images/myself/room.png"
-								size="60rpx"></u-icon>
-							<text class="grid-text">{{ item.name }}</text>
-							<view class="device-info" v-if="item.devices.length">
-								<text class="grid-text-device">{{ (getDevices(item).name || '未命名设备')  + ' | '}}</text>
-								<text class="grid-text-device"
-									:style="{color: getDevices(item).onlineFlag == 1 ? 'rgb(13, 171, 28)' : 'rgb(255, 72, 0)'}">
-									{{(getDevices(item).onlineFlag == 1 ? '在线' : '离线')}}</text>
-							</view>
-							<text class="grid-text-device" v-else>未绑定设备</text>
-							<u-icon @click.native.stop="onDelete(item.roomId)" class="ui-close active"
-								name="close-circle-fill" size="40rpx">
-							</u-icon>
-						</view>
-						<view class="ui-menu-btn">
-							<button v-if="!item.devices.length" class="default" @click="binding(item)">绑定</button>
-							<button v-else class="plain" @click="unbinding(item.devices)">解绑</button>
-						</view>
-					</u-grid-item>
-				</u-grid>
+		<app-body :bg="false">
+			<view class="ui-logo">
+				<view class="logo-text">
+					{{ familyInfo.name }}
+				</view>
+				<view class="action" @click="openFamilyEdit">
+					<u-icon name="/static/images/room-edit.png" size="28"></u-icon>
+					<text>编辑</text>
+				</view>
 			</view>
-			<view class="ui-btn"><button class="default" @click="add">创建房间</button></view>
+
+			<view class="ui-menu">
+				<view class="ui-menu-item" v-for="(item, index) in list" :key="index">
+					<view class="item-box" @click="openRoomEdit(item)">
+						<view class="device-status">
+							<text class="online" v-if="getDevices(item).onlineFlag == 1">在线</text>
+							<text class="offline" v-else>离线</text>
+						</view>
+						<view class="device-info">
+							<image src="/static/images/leida-nm.png"></image>
+							<view class="detail">
+								<text class="name">{{ item.name }}</text>
+								<text class="position" v-if="item.devices.length">
+									{{ item.devices[0].roomName + ' | ' + item.devices[0].location}}
+								</text>
+								<text class="position" v-else>
+									未绑定设备
+								</text>
+							</view>
+						</view>
+						<view class="device-action">
+							<text class="danger" @click.native.stop="onDelete(item.roomId)">删除</text>
+							<text class="warn" v-if="!item.devices.length" @click="binding(item)">绑定</text>
+							<text class="orange" v-else @click="unbinding(item.devices)">解绑</text>
+						</view>
+					</view>
+				</view>
+			</view>
+			<view class="ui-btn"><button class="default" @click="add">添加</button></view>
 			<room-pop ref="addRoom" :mode="roomMode" @update="handleInitList" />
 			<family-pop ref="addFamily" mode="edit" @update="familyNext" />
 			<bind-device :payload="bindPayload" @next="handleInitList" ref="bindDev" />
@@ -199,114 +208,255 @@
 </script>
 
 <style lang="scss">
-	.ui-logo-icon {
-		margin-top: 30rpx;
+	.online {
+		background-image: linear-gradient(90deg, #1EC862 0%, #13B98F 100%);
 	}
 
-	.ui-menu {
-		margin-top: 70rpx;
-		padding: 0 78rpx;
-		min-height: 600rpx;
-
-		.ui-menu-item {
-			position: relative;
-			margin-top: 58rpx;
-			display: flex;
-			align-items: center;
-			justify-content: center;
-			flex-direction: column;
-			font-size: 27rpx;
-			color: #414141;
-			height: 258rpx;
-			width: 258rpx;
-			border-radius: 10rpx;
-			filter: drop-shadow(7.824rpx 10.382rpx 8rpx rgba(7, 5, 5, 0.08));
-			background-image: linear-gradient(96deg, #f5f5f5 0%, #e5e5e5 100%);
-			text-align: center;
-
-			.grid-text {
-				display: inline-flex;
-				align-items: center;
-				justify-content: center;
-				margin: 20rpx 0 10rpx 0;
-				width: 70%;
-				height: 60rpx;
-				font-size: 32rpx;
-				color: #414141;
-			}
-
-			.grid-text-device {
-				color: #999;
-			}
-
-			.ui-edit {
-				position: absolute;
-				bottom: 10rpx;
-				right: 10rpx;
-				z-index: 10;
-			}
-
-			.ui-close {
-				position: absolute;
-				right: -10rpx;
-				top: -10rpx;
-			}
-		}
-	}
-
-	.ui-btn {
-		text-align: center;
-		margin: 50rpx 0;
-
-		button {
-			width: 276rpx;
-			height: 74rpx;
-			font-size: 30rpx;
-			line-height: 74rpx;
-			border-radius: 60rpx;
-		}
-	}
-
-	.ui-menu-btn {
-		margin-top: 10rpx;
-		text-align: center;
-		width: 100%;
-
-		button {
-			height: 60rpx;
-			width: 258rpx;
-			line-height: 60rpx;
-			border-radius: 10px;
-			font-size: 30rpx;
-		}
+	.offline {
+		background: #D4D4D4;
 	}
 
 	.ui-logo {
-		height: 69rpx;
-		// width: 366rpx;
-		font-size: 69rpx;
-		color: #fff;
-		margin: 20rpx 72rpx;
-		display: inline-block;
-		font-weight: bold;
-	}
-
-	.ui-slider {
-		width: 100%;
+		background: #ffffff;
+		padding: 50rpx 40rpx;
+		padding-bottom: 50rpx;
 		display: flex;
-		flex-direction: row;
 		align-items: center;
 		justify-content: space-between;
 
-		text {
-			font-size: 26rpx;
-			color: #999;
+		.logo-text {
+			font-size: 37px;
+			font-weight: bold;
 		}
 
-		&>* {
-			:nth-child(1) {
-				width: 320rpx;
+		.action {
+			display: flex;
+			align-items: center;
+
+			text {
+				font-size: 26rpx;
+				color: #353535;
 			}
+		}
+	}
+
+	.ui-menu {
+		padding: 30rpx 20rpx;
+		display: flex;
+		flex-wrap: wrap;
+		box-sizing: border-box;
+
+		.ui-menu-item {
+			background: #FFFFFF;
+			border-radius: 16px;
+			width: 48%;
+			margin: 0 1%;
+			margin-bottom: 20rpx;
+			position: relative;
+
+			.item-box {
+				padding: 30rpx 20rpx;
+				display: flex;
+				flex-direction: column;
+				justify-content: center;
+				align-items: center;
+
+				.device-status {
+					position: absolute;
+					top: 18rpx;
+					right: 18rpx;
+
+					text {
+						color: #FFFFFF;
+						font-size: 20rpx;
+						padding: 4rpx 8rpx;
+						border-radius: 4px;
+					}
+				}
+
+				.device-info {
+					display: flex;
+					align-items: center;
+					justify-content: space-between;
+
+					image {
+						height: 120rpx;
+						width: 120rpx;
+					}
+
+					.detail {
+						flex: 1;
+						margin-left: 10rpx;
+						text {
+							display: inline-block;
+						}
+
+						.name {
+							font-size: 36rpx;
+							color: #353535;
+							font-weight: 500;
+						}
+
+						.position {
+							margin-top: 10rpx;
+							font-size: 26rpx;
+							color: #888888;
+						}
+					}
+				}
+
+				.device-action {
+					width: 100%;
+					margin-top: 20rpx;
+					display: flex;
+					justify-content: space-around;
+
+					.danger {
+						color: #E95656;
+					}
+
+					.warn {
+						border: 1px solid rgba(254, 174, 67, 1);
+						border-radius: 24px;
+						color: #FEAE43;
+						padding: 4rpx 18rpx;
+						font-size: 24rpx;
+					}
+
+					.orange {
+						padding: 4rpx 18rpx;
+						font-size: 24rpx;
+						color: #fff;
+						background-image: linear-gradient(90deg, #FFB24D 0%, #FD913B 100%);
+						border-radius: 24px;
+					}
+
+					text {
+						font-size: 26rpx
+					}
+				}
+			}
+		}
+	}
+
+	// .ui-logo-icon {
+	// 	margin-top: 30rpx;
+	// }
+
+	// .ui-menu {
+	// 	margin-top: 70rpx;
+	// 	padding: 0 78rpx;
+	// 	min-height: 600rpx;
+
+	// 	.ui-menu-item {
+	// 		position: relative;
+	// 		margin-top: 58rpx;
+	// 		display: flex;
+	// 		align-items: center;
+	// 		justify-content: center;
+	// 		flex-direction: column;
+	// 		font-size: 27rpx;
+	// 		color: #414141;
+	// 		height: 258rpx;
+	// 		width: 258rpx;
+	// 		border-radius: 10rpx;
+	// 		filter: drop-shadow(7.824rpx 10.382rpx 8rpx rgba(7, 5, 5, 0.08));
+	// 		background-image: linear-gradient(96deg, #f5f5f5 0%, #e5e5e5 100%);
+	// 		text-align: center;
+
+	// 		.grid-text {
+	// 			display: inline-flex;
+	// 			align-items: center;
+	// 			justify-content: center;
+	// 			margin: 20rpx 0 10rpx 0;
+	// 			width: 70%;
+	// 			height: 60rpx;
+	// 			font-size: 32rpx;
+	// 			color: #414141;
+	// 		}
+
+	// 		.grid-text-device {
+	// 			color: #999;
+	// 		}
+
+	// 		.ui-edit {
+	// 			position: absolute;
+	// 			bottom: 10rpx;
+	// 			right: 10rpx;
+	// 			z-index: 10;
+	// 		}
+
+	// 		.ui-close {
+	// 			position: absolute;
+	// 			right: -10rpx;
+	// 			top: -10rpx;
+	// 		}
+	// 	}
+	// }
+
+	// .ui-btn {
+	// 	text-align: center;
+	// 	margin: 50rpx 0;
+
+	// 	button {
+	// 		width: 276rpx;
+	// 		height: 74rpx;
+	// 		font-size: 30rpx;
+	// 		line-height: 74rpx;
+	// 		border-radius: 60rpx;
+	// 	}
+	// }
+
+	// .ui-menu-btn {
+	// 	margin-top: 10rpx;
+	// 	text-align: center;
+	// 	width: 100%;
+
+	// 	button {
+	// 		height: 60rpx;
+	// 		width: 258rpx;
+	// 		line-height: 60rpx;
+	// 		border-radius: 10px;
+	// 		font-size: 30rpx;
+	// 	}
+	// }
+
+	// .ui-logo {
+	// 	height: 69rpx;
+	// 	// width: 366rpx;
+	// 	font-size: 69rpx;
+	// 	color: #fff;
+	// 	margin: 20rpx 72rpx;
+	// 	display: inline-block;
+	// 	font-weight: bold;
+	// }
+
+	// .ui-slider {
+	// 	width: 100%;
+	// 	display: flex;
+	// 	flex-direction: row;
+	// 	align-items: center;
+	// 	justify-content: space-between;
+
+	// 	text {
+	// 		font-size: 26rpx;
+	// 		color: #999;
+	// 	}
+
+	// 	&>* {
+	// 		:nth-child(1) {
+	// 			width: 320rpx;
+	// 		}
+	// 	}
+	// }
+	.ui-btn {
+		width: 100%;
+		position: absolute;
+		bottom: 0;
+		left: 0;
+
+		button {
+			border-radius: 0rpx !important;
 		}
 	}
 </style>
