5febdf422b767a941e1f415182fb4e81e1e283f2
linj <1017541912@qq.com>
2023-02-23 09:46:58 +0800

    1、tcp 平台下发接口示例提交

--- backend/varyar-tcp/src/main/java/com/newlandnpt/varyar/tcp/gateway/controller/DevicePhoneController.java
+++ backend/varyar-tcp/src/main/java/com/newlandnpt/varyar/tcp/gateway/controller/DevicePhoneController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 @RequestMapping("/tcp/device/phone")
-public class DeviceConfigController {
+public class DevicePhoneController {
 
     @Autowired
     private IDevicePhoneService devicePhoneService;
