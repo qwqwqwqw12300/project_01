3b8025e4e57aca4a14cb119b9de2de9b692d1981
li.jx <296651310@qq.com>
2023-03-02 17:06:27 +0800

    (*) 修改获取亲情号码的返回模式

--- backend/varyar-common/src/main/java/com/newlandnpt/varyar/common/core/domain/entity/DevicePhone.java
+++ backend/varyar-common/src/main/java/com/newlandnpt/varyar/common/core/domain/entity/DevicePhone.java
@@ -11,6 +11,7 @@ import java.io.Serializable;
 public class DevicePhone implements Serializable
 {
     private static final long serialVersionUID = 1L;
+    public String button;
     /**
      * 联系人名称
      * */
@@ -23,10 +24,19 @@ public class DevicePhone implements Serializable
     public DevicePhone() {
     }
 
-    public DevicePhone(String phoneName, String phone) {
+    public DevicePhone(String button) {
+        this.button = button;
+    }
+
+    public DevicePhone(String button, String phoneName, String phone) {
+        this.button = button;
         this.phoneName = phoneName;
         this.phone = phone;
     }
+
+
+
+
 
     public String getPhoneName() {
         return phoneName;
@@ -44,4 +54,11 @@ public class DevicePhone implements Serializable
         this.phone = phone;
     }
 
+    public String getButton() {
+        return button;
+    }
+
+    public void setButton(String button) {
+        this.button = button;
+    }
 }
