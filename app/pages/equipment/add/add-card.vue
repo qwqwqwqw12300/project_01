811ccdb725caefca6f8921a1dc8fb162bbe13ecb
youh <youh123!>
2023-03-09 09:13:36 +0800

    chore: 微调

--- app/pages/equipment/add/add-card.vue
+++ app/pages/equipment/add/add-card.vue
@@ -55,7 +55,6 @@
 				uni.scanCode({
 					// scanType: ['barCode', 'qrCode'],
 					success: res => {
-						console.log(res)
 						const {
 							scanType,
 							result
@@ -68,32 +67,20 @@
 				});
 			},
 			handleSubmit() {
-				uni.getLocation({
-					geocode: true,
-					type: 'gcj02',
-					latitude: '119.406043',
-					longitude: '26.0185753',
-					// type: 'wgs84',
-					success: function(res) {
-						console.log(res, '0099')
-						console.log('当前位置的经度：' + res.longitude);
-						console.log('当前位置的纬度：' + res.latitude);
-					}
-				});
-				// const {
-				// 	deviceNo,
-				// 	deviceName
-				// } = this.cardInfo
-				// if (!deviceNo) return uni.$u.toast('设备码不能为空')
-				// if (!deviceName) return uni.$u.toast('设备名称不能为空')
-				// PostcreDevice({
-				// 	...this.cardInfo
-				// }).then(res => {
-				// 	uni.$u.toast(res.msg)
-				// 	setTimeout(() => {
-				// 		uni.navigateBack()
-				// 	}, 1000)
-				// })
+				const {
+					deviceNo,
+					deviceName
+				} = this.cardInfo
+				if (!deviceNo) return uni.$u.toast('设备码不能为空')
+				if (!deviceName) return uni.$u.toast('设备名称不能为空')
+				PostcreDevice({
+					...this.cardInfo
+				}).then(res => {
+					uni.$u.toast(res.msg)
+					setTimeout(() => {
+						uni.navigateBack()
+					}, 1000)
+				})
 			}
 		}
 	}
