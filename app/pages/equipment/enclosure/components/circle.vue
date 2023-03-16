69c8ff3a79b3ed5a7f8c0cdf954f122410406ec9
youh <youh123!>
2023-03-16 14:24:10 +0800

    chore: 电子围栏圆形模块调整

--- app/pages/equipment/enclosure/components/circle.vue
+++ app/pages/equipment/enclosure/components/circle.vue
@@ -1,7 +1,7 @@
 <template>
 	<view>
-		<circle-map :record="sliderValue"></circle-map>
-		<touch-popup :minHeight="0.1" :maxHeight="0.7" :touchHeight="64" radius="30rpx">
+		<circle-map :record="mapInfo"></circle-map>
+		<touch-popup :minHeight="0.1" :maxHeight="0.75" :touchHeight="64" radius="30rpx">
 			<view class="title">
 				电子围栏设置
 			</view>
@@ -18,11 +18,11 @@
 					半径
 				</text>
 				<view class="slider">
-					<u-slider @change="onSlider" max="500" v-model="sliderValue" activeColor="#eeaa3d"
+					<u-slider @change="onSlider" max="500" v-model="mapInfo.sliderValue" activeColor="#eeaa3d"
 						blockColor="#eeaa3d" inactiveColor="#c0c4cc" />
 				</view>
 				<text class="value" style="text-align: center;">
-					{{sliderValue}}米
+					{{mapInfo.sliderValue}}米
 				</text>
 			</view>
 			<view class="address-list">
@@ -37,6 +37,9 @@
 					<u-icon :name="getMapIcon(n)" size="44rpx" style="margin-right: 6rpx;" />
 				</view>
 			</view>
+			<view class="ui-btn">
+				<button class="default" @click="handleSubmit">提交</button>
+			</view>
 		</touch-popup>
 	</view>
 </template>
@@ -45,13 +48,26 @@
 	import {
 		mapState,
 	} from 'vuex';
+	import {
+		isIos,
+		isApp
+	} from '@/common/utils/util';
+	let mapSearch;
+	if (isApp()) mapSearch = weex.requireModule('mapSearch');
+	import {
+		PostAddFence,
+	} from '@/common/http/api.js';
 	export default {
 		data() {
 			return {
 				siteInfo: '',
-				sliderValue: 0,
 				currentSelect: '',
 				poiList: [], //搜索地址
+				mapInfo: {
+					sliderValue: 200,
+					latitude: '',
+					longitude: '',
+				}
 			}
 		},
 		computed: {
@@ -65,16 +81,144 @@
 				}
 			},
 		},
+		created() {
+			uni.$on('searchData', data => {
+				console.log(data, 'dddddd----------')
+				this.mapMarker(data)
+			})
+			this.handleGetLocation()
+		},
 		methods: {
 			mapMarker(data) {
-
+				const {
+					province,
+					city,
+					district,
+					address,
+					index,
+					location: {
+						latitude,
+						longitude
+					}
+				} = data
+				this.currentSelect = index
+				this.siteInfo = province + city + district + address
+				this.mapInfo = {
+					sliderValue: 200,
+					latitude,
+					longitude,
+				}
 			},
+			/**
+			 * 跳转搜索
+			 */
 			goSearch() {
-
+				uni.navigateTo({
+					url: '/pages/equipment/search'
+				})
 			},
 			onSlider() {
 
-			}
+			},
+			/**
+			 * 获取当前定位
+			 */
+			handleGetLocation() {
+				uni.showLoading({
+					title: '加载中'
+				})
+				try {
+					uni.getLocation({
+						geocode: true,
+						type: isIos() ? 'wgs84' : 'gcj02',
+						success: (res) => {
+							this.getSiteInfo(res)
+							const {
+								latitude,
+								longitude,
+								address: {
+									province,
+									city,
+									district,
+									street
+								},
+							} = res
+							this.latitude = latitude
+							this.longitude = longitude
+							this.siteInfo = province + city + district + street
+							this.mapInfo = {
+								sliderValue: 200,
+								latitude,
+								longitude,
+							}
+							uni.hideLoading()
+						},
+						false: (res) => {
+							console.log(res, 'error')
+							uni.hideLoading()
+						}
+					})
+				} catch (e) {
+					console.log(e, '代码报错--------');
+					//TODO handle the exception
+				}
+
+			},
+			/**
+			 * 获取周围位置信息
+			 */
+			getSiteInfo(data) {
+				const {
+					latitude,
+					longitude
+				} = data
+				mapSearch && mapSearch.poiSearchNearBy({
+					point: {
+						latitude,
+						longitude
+					},
+					key: '小区'
+				}, res => {
+					const {
+						poiList
+					} = res;
+					if (poiList && poiList.length) {
+						this.poiList = (poiList.length > 4 ? poiList.slice(0, 4) : poiList).map((n, i) => {
+							n.index = i
+							return n
+						})
+					}
+				})
+			},
+			/**
+			 * 保存
+			 */
+			handleSubmit() {
+				const {
+					// deviceId,
+					siteInfo: address,
+					mapInfo: {
+						longitude,
+						latitude,
+						sliderValue,
+					}
+				} = this
+				if (sliderValue === 0) {
+					return uni.$u.toast('半径长度大于0')
+				}
+				PostAddFence({
+					fenceType: 'circle',
+					deviceNo: this.deviceInfo.no,
+					deviceId: this.deviceInfo.deviceId,
+					address,
+					longitude,
+					latitude,
+					radius: sliderValue + '',
+				}).then(res => {
+					uni.navigateBack()
+					uni.$u.toast(res.msg)
+				})
+			},
 		}
 	}
 </script>
@@ -168,5 +312,8 @@
 				color: #353535;
 			}
 		}
+	}
+	.ui-btn{
+		margin-top: 20rpx;
 	}
 </style>
