9fe5d460217e37cf216afa2ed9abd5574c4d681f
youh <youh123!>
2023-03-23 17:04:22 +0800

    chore: 添加详情数据

--- app/pages/health/exercise/exercise.vue
+++ app/pages/health/exercise/exercise.vue
@@ -7,6 +7,9 @@
 		<view class="ui-echart">
 			<exexcise-echart></exexcise-echart>
 		</view>
+		<view class="day-title">
+			<text>日平均数据</text>
+		</view>
 		<view class="ui-tab">
 			<scroll-view class="scroll" scroll-x>
 				<block v-for="(item,index) in list" :key="index">
@@ -22,6 +25,31 @@
 					</view>
 				</block>
 			</scroll-view>
+		</view>
+		<view class="detail-title">
+			详情
+		</view>
+		<view class="ui-detail">
+			<view class="detail-box">
+				<view class="box-cell">
+					<view class="title">
+						20:37 - 20:53 步行
+					</view>
+					<view class="content">
+						<view class="c">
+							<u-icon size="30" name="/static/images/bushu.png" />
+							<text>97</text>步
+						</view>
+						<view class="c">
+							<u-icon size="40rpx" name="arrow-left" />
+							<text>0.1</text>公里
+						</view>
+						<view class="c">
+							<text>0.1</text>千卡
+						</view>
+					</view>
+				</view>
+			</view>
 		</view>
 	</app-body>
 </template>
@@ -84,6 +112,26 @@
 
 	.ui-echart {}
 
+	.day-title {
+		margin-top: 30rpx;
+		height: 50rpx;
+		line-height: 50rpx;
+		padding: 0 32rpx;
+		font-size: 32rpx;
+		color: #353535;
+		font-weight: 550;
+	}
+
+	.detail-title {
+		margin-top: 30rpx;
+		height: 50rpx;
+		line-height: 50rpx;
+		padding: 0 32rpx;
+		font-size: 32rpx;
+		color: #353535;
+		font-weight: 550;
+	}
+
 	.ui-tab {
 		margin-top: 30rpx;
 		width: 100%;
@@ -133,5 +181,53 @@
 		height: 0 !important;
 		-webkit-appearance: none;
 		background: transparent;
+	}
+
+	.ui-detail {
+		padding: 0 32rpx;
+		margin-top: 20rpx;
+
+		.detail-box {
+			background: #FFFFFF;
+			border-radius: 16rpx;
+			padding: 30rpx 20rpx;
+
+			.box-cell {
+				.title {
+					height: 50rpx;
+					line-height: 50rpx;
+					font-size: 30rpx;
+					color: #353535;
+				}
+
+				.content {
+					height: 120rpx;
+					display: flex;
+					align-items: center;
+					justify-content: space-between;
+					border-bottom: solid 2px #f7f7f7;
+
+					&:last-child {
+						border: none !important;
+					}
+
+					.c {
+
+						display: flex;
+						align-items: center;
+						font-size: 26rpx;
+						color: #888888;
+
+						text {
+							margin-left: 10rpx;
+							font-size: 40rpx;
+							color: #353535;
+							font-weight: 600;
+						}
+					}
+				}
+
+			}
+		}
 	}
 </style>
