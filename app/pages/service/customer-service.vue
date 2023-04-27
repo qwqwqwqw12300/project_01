3e72321253068ca8add64ba4fc0d90d64fd4754a
youh <youh123!>
2023-04-27 15:20:44 +0800

    chore: 修复客服链接失败问题

--- app/pages/service/customer-service.vue
+++ app/pages/service/customer-service.vue
@@ -7,18 +7,24 @@
 
 <template>
 	<view>
+		<view :crispWebsoteId="crispWebsoteId" :change:crispWebsoteId="service.loadData"></view>
 		<u-loading-page loading-text="正在为您连接客服,请稍后..." :loading="true"></u-loading-page>
 	</view>
 </template>
 
 <script>
+	import {
+		env
+	} from "@/config/env.js";
 	export default {
+		mounted() {
+			this.crispWebsoteId = env.crispWebsoteId;
+		},
 		data() {
 			return {
-
+				crispWebsoteId: "",
 			};
-		}
-
+		},
 	};
 </script>
 
@@ -26,20 +32,23 @@
 	import {
 		Crisp
 	} from "crisp-sdk-web";
-	import {
-		env
-	} from "@/config/env.js";
 	export default {
 		mounted() {
-			Crisp.configure(env.crispWebsoteId, {
-				lockMaximized: true // 锁定页面不关闭
-			});
-			Crisp.user.setNickname("test1"); // 设置昵称
-			Crisp.session.setData({ // 其他携带的数据
-				user_id: "123456"
-			});
-			$crisp.push(["safe", true])
-			Crisp.chat.open();
+
+		},
+		methods: {
+			loadData(crispWebsoteId) {
+				if (!crispWebsoteId) return
+				Crisp.configure(crispWebsoteId, {
+					lockMaximized: true // 锁定页面不关闭
+				});
+				Crisp.user.setNickname("test1"); // 设置昵称
+				Crisp.session.setData({ // 其他携带的数据
+					user_id: "123456"
+				});
+				$crisp.push(["safe", true])
+				Crisp.chat.open();
+			}
 		}
 	}
 </script>
