729af36553c22eab09738704bd1f6a31b7811ce0
CCC <1957370141@qq.com>
2023-09-14 21:11:42 +0800

    健康页面周图表柱状图点击获取实时值实现

--- app/pages/health/sleep/components/sleep-week.vue
+++ app/pages/health/sleep/components/sleep-week.vue
@@ -3,28 +3,29 @@
 		<!-- <view class="ui-totalTime">00:00</view> -->
 		<view class="ui-time">
 			<view>
-				<text class="ui-font">{{ totalData.hour }}</text>
-				<text class="ui-time-font">时</text>
+				<!-- <text class="ui-font">{{ totalData.hour }}</text> -->
+				<text class="ui-time-font" v-if="hour"> {{hour}} 时</text>
+
 			</view>
 			<view style="margin-left: 10rpx;">
-				<text class="ui-font">{{ totalData.minutes }}</text>
-				<text class="ui-time-font">分</text>
+				<!-- <text class="ui-font">{{ totalData.minutes }}</text> -->
+				<text class="ui-time-font" v-if="hour">{{minute}}分</text>
 			</view>
 		</view>
-		<view class="ui-title">
+		<!-- <view class="ui-title">
 			<view class="ui-circle"></view>
 			<view class="ui-title-font">睡眠时长</view>
-		</view>
+		</view> -->
 		<view class="ui-window">
-			<view>
+			<!-- <view>
 				<view class="ui-content" v-for="(item,index) in sleepList" :key="index">
 					<view class="ui-content-title">
 						<view class="ui-content-circle" :style="{backgroundColor:item.color}"></view>
 						<view class="ui-content-font">{{item.title}}</view>
 					</view>
 				</view>
-			</view>
-			<app-echarts :option="option" id="myChart" class="myChart"></app-echarts>
+			</view> -->
+			<app-echarts :option="option" id="myChart" class="myChart" @Cclick="clickEchart"></app-echarts>
 		</view>
 		<WatchDiv :text="'日均睡眠得分'" :content="sleepScore"></WatchDiv>
 	</view>
@@ -80,6 +81,10 @@
 					minutes: 0,
 				},
 				sleepScore: 0,
+				Data: [],
+				spMapList: [],
+				hour: '',
+				minute: ''
 			}
 		},
 		computed: {
@@ -104,6 +109,17 @@
 			}
 		},
 		methods: {
+			clickEchart(val) {
+				// console.log(val, 'val');
+				const res = this.Data.find(item => {
+					return item[0] == val.value
+				})
+
+				this.hour = Math.floor(res[1] / 60)
+				console.log(this.hour, 'aaaaaaaaaa');
+				this.minute = res[1] % 60
+				// console.log(this.minute);
+			},
 			handleInit() {
 				const {
 					deviceId,
@@ -113,32 +129,42 @@
 					deviceId,
 					humanId,
 					beginDate: this.dayTime[0],
-					endDate: this.dayTime[this.dayTime.length-1]
+					endDate: this.dayTime[this.dayTime.length - 1]
 				}).then(res => {
-					const {
-						sleepSegmentationDataList,
-						sleepScore,
-						countTime
-					} = res.data
-					this.sleepScore = sleepScore
-					this.totalData.hour = countTime[0]
-					this.totalData.minutes = countTime[1] % 60
-					const data = sleepSegmentationDataList.map(n => {
-						n.differTime = Number(this.getDifferTime(n.st, n.et).toFixed(2))
-						n.time = uni.$u.timeFormat(n.st, 'yyyy-mm-dd')
-						return n
+					// const {
+					// 	sleepSegmentationDataList,
+					// 	sleepScore,
+					// 	countTime
+					// } = res.data
+					// this.sleepScore = sleepScore
+					// this.totalData.hour = countTime[0]
+					// this.totalData.minutes = countTime[1] % 60
+					// const data = sleepSegmentationDataList.map(n => {
+					// 	n.differTime = Number(this.getDifferTime(n.st, n.et).toFixed(2))
+					// 	n.time = uni.$u.timeFormat(n.st, 'yyyy-mm-dd')
+					// 	return n
+					// })
+					this.Data = res.data.MapList.map(n => { //获取当前周的所有max值
+						return [n.time, n.slt]
 					})
-
-					this.dayTime.forEach((n, index) => {
-						const dayData = data.filter(item => {
-							return item.time === n
-						})
-						this.arr.forEach(e => {
-							this.sleepData[e][index] = dayData.reduce((p, c) => {
-								return p + ((c.slt === e) ? c.differTime : 0)
-							}, 0).toFixed(2)
-						})
-					})
+					this.spMapList = []
+					for (let i = 0; i < res.data.MapList.length; i++) {
+						this.spMapList.push([
+							res.data.MapList[i].time,
+							res.data.MapList[i].slt
+						])
+					}
+					console.log(this.spMapList, 'a啊啊啊啊啊啊啊啊啊啊');
+					// this.dayTime.forEach((n, index) => {
+					// 	const dayData = data.filter(item => {
+					// 		return item.time === n
+					// 	})
+					// 	this.arr.forEach(e => {
+					// 		this.sleepData[e][index] = dayData.reduce((p, c) => {
+					// 			return p + ((c.slt === e) ? c.differTime : 0)
+					// 		}, 0).toFixed(2)
+					// 	})
+					// })
 
 					this.logstatrt()
 
@@ -146,77 +172,139 @@
 			},
 			logstatrt() {
 				this.option = {
-					// backgroundColor: '#fff',
-					color: ["#F1C44F", "#EF7B8C", "#BF47CA", "#8538E0"],
-					legend: {
-						data: ['', '']
-
+					title: {
+						text: ''
 					},
 					tooltip: {
+						positionStatus: true,
+						showContent: false,
 						trigger: 'axis',
 						axisPointer: {
-							type: 'shadow'
-						}
+							type: 'line',
+							lineStyle: {
+								type: 'dashed', //指示线为虚线
+							},
+						},
+					},
+					backgroundColor: '#fff',
+					legend: {
+						data: []
 					},
 					grid: {
-						left: '0',
-						right: '0',
+						left: '20',
+						right: '20',
 						bottom: '5',
-						top: '0',
+						top: '20',
 						containLabel: true
 					},
+					toolbox: {},
 					xAxis: [{
-						type: 'category',
+						type: 'time',
+						boundaryGap: false,
+						interval: 24 * 3600 * 1000,
 						data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日', ],
 						axisTick: {
 							show: false
 						},
-						axisLine: {
-							show: false
+						axisPointer: {
+							handle: {
+								show: true,
+								size: 18,
+								margin: 2,
+								throttle: 5,
+								color: '#ffac4a'
+							}
 						},
-						axisTick: {
-							alignWithLabel: true
-						}
-					}],
-					yAxis: [{
-						type: 'value',
+						axisLine: {
+							lineStyle: {
+								color: 'rgb(238,238,238)',
+								width: 1
+							}
+						},
 						axisLabel: {
+							margin: 13,
+							textStyle: {
+								color: "#666"
+							},
+							formatter: 'week'
+						},
+						splitLine: {
 							show: false
 						},
-						axisTick: {
-							show: false
+					}],
+					yAxis: {
+						type: "value",
+						min: '0',
+						max: '720',
+						scale: true,
+						splitArea: {
+							show: true,
+							areaStyle: {
+								color: ['#f6f8fc', '#fff']
+							}
+						},
+						axisLabel: {
+							textStyle: {
+								color: "#666"
+							}
+						},
+						nameTextStyle: {
+							color: "#666",
+							fontSize: 12,
+							lineHeight: 40
+						},
+						// 分割线
+						splitLine: {
+							lineStyle: {
+								type: "dashed",
+								color: "#E9E9E9"
+							}
 						},
 						axisLine: {
 							show: false
 						},
-					}],
-					series: [{
-							name: '清醒',
-							type: 'bar',
-							stack: '总量',
-							barWidth: '70%',
-							data: this.sleepData[0]
-						}, {
-							name: '快速眼动',
-							type: 'bar',
-							stack: '总量',
-							barWidth: '70%',
-							data: this.sleepData[1]
-						},
-						{
-							name: '浅睡',
-							type: 'bar',
-							stack: '总量',
-							barWidth: '70%',
-							data: this.sleepData[2]
-						}, {
-							name: '深睡',
-							type: 'bar',
-							stack: '总量',
-							barWidth: '70%',
-							data: this.sleepData[3]
+						axisTick: {
+							show: false
 						}
-					]
+					},
+					series: [{
+						barWidth: '25%', //柱形图宽度
+						type: 'bar',
+						stack: 'Total',
+						// label: {
+						// 	show: true,
+						// 	position: 'top'
+						// },
+						showSymbol: false,
+						itemStyle: {
+							normal: {
+								color: 'rgba(255,89,89,1)',
+								barBorderRadius: 30,
+								lineStyle: {
+									color: "rgba(255,89,89,1)",
+									width: 1
+								},
+								areaStyle: {
+									color: {
+										type: 'linear',
+										x: 0,
+										y: 0,
+										x2: 0,
+										y2: 1,
+										colorStops: [{
+											offset: 0,
+											color: 'rgba(255,89,89,0.90)'
+										}, {
+											offset: 1,
+											color: 'rgba(255,89,89,0.00)'
+										}],
+										global: false
+									}
+								}
+							}
+						},
+						data: this.spMapList
+					}]
 				};
 			},
 			getDifferTime(start, end) {
@@ -269,6 +357,7 @@
 		display: flex;
 		align-items: center;
 		justify-content: space-between;
+		padding: 0 16px;
 
 		.ui-content {
 			width: 48%;
